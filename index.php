91589ef3ce148cfca25fdb1804a21b22f225ade9
DWVoid <yshliu0321@icloud.com>
2019-04-16 22:10:21 +0800

    Add Standalone xqr33 page

--- index.php
+++ index.php
@@ -34,7 +34,7 @@
     <div class="link"><a href="">XQR3.3</a></div>
     中等容量Minecraft 1.12.2 Mod服务器，搭载一系列科学及魔法Mod……
     <br />
-    <a href="http://139.99.6.26:8123">在线地图</a>
+    <a href="xqr33.php">在线地图</a>
     <a href="">地址：139.99.6.26</a>
 </div>
 
---
+++ xqr33.php
@@ -0,0 +1,7 @@
+<?php include_once('template.php'); loadHeader(); ?>
+
+<div class="main_content">
+    <iframe src="http://139.99.6.26:8123"></iframe>
+</div>
+
+<?php loadFooter(); ?>

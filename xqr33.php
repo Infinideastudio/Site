bdd734a62c5d81bbafbb9657733033ef82718819
DWVoid <yshliu0321@icloud.com>
2019-04-18 05:31:11 +0800

    Add Redirect

--- xqr33.php
+++ xqr33.php
@@ -1,7 +1,9 @@
 <?php include_once('template.php'); loadHeader(); ?>
 
 <div class="main_content">
-    <iframe src="/common/redirect.php?url=http://139.99.6.26:8123"></iframe>
+    <div class="iframe-container">
+        <iframe src="//xqr3.newinfinideas.org"></iframe>
+    </div>
 </div>
 
 <?php loadFooter(); ?>
